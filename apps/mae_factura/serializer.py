41af34f84ad2b810aad9cc586cfa75d3238f2ab6
ES-CH <stromcaceres4@gmail.com>
2024-07-12 12:01:21 -0500

    Update MaeFacturaSerializer to MaeFacturaListSerializer in serializer.py and add update method in views.py

--- apps/mae_factura/serializer.py
+++ apps/mae_factura/serializer.py
@@ -5,13 +5,26 @@ from apps.empresa.serializer import EmpresaSerializer
 from apps.mae_factura.models import MaeFactura
 
 
-class MaeFacturaSerializer(serializers.ModelSerializer):
+class MaeFacturaListSerializer(serializers.ModelSerializer):
     empresa = EmpresaSerializer(read_only=True)
     cliente = ClienteSerializer(read_only=True)
     fehca_creacion = serializers.DateTimeField(
         format='%Y-%m-%d %H:%M', read_only=True)
     fecha_auditoria = serializers.DateTimeField(
         format='%Y-%m-%d %H:%M', read_only=True)
+
+    class Meta:
+        model = MaeFactura
+        fields = '__all__'
+
+
+class MaeFacturaSerializer(serializers.ModelSerializer):
+    empresa = EmpresaSerializer(read_only=True)
+    cliente = ClienteSerializer(read_only=True)
+    fehca_creacion = serializers.DateTimeField(
+        format='%Y-%m-%d', read_only=True)
+    fecha_auditoria = serializers.DateTimeField(
+        format='%Y-%m-%d', read_only=True)
 
     class Meta:
         model = MaeFactura
--- apps/mae_factura/views.py
+++ apps/mae_factura/views.py
@@ -2,7 +2,7 @@ from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from apps.mae_factura.models import MaeFactura
-from apps.mae_factura.serializer import (MaeFacturaCreateSerializer,
+from apps.mae_factura.serializer import (MaeFacturaCreateSerializer, MaeFacturaListSerializer,
                                          MaeFacturaSerializer)
 
 # Create your views here.
@@ -13,7 +13,9 @@ class MaeFacturaViewSet(viewsets.ModelViewSet):
     serializer_class = MaeFacturaSerializer
 
     def get_serializer_class(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action == 'list':
+            return MaeFacturaListSerializer
+        if self.action == 'retrieve':
             return MaeFacturaSerializer
         else:
             return MaeFacturaCreateSerializer
@@ -23,5 +25,14 @@ class MaeFacturaViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = MaeFacturaCreateSerializer(instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
